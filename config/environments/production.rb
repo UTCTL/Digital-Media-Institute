a350067005fb311570f02332333a2c1570cc4ef9
C. Scott Herrick <csherrick@gmail.com>
2012-03-15 09:12:29 -0500

    Added gmail smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,17 +47,6 @@ Dmtraining::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( s3_upload.js )
 
-  config.action_mailer.smtp_settings = {
-                                          :address        => 'smtp.sendgrid.net',
-                                          :port           => '587',
-                                          :authentication => :plain,
-                                          :user_name      => ENV['SENDGRID_USERNAME'],
-                                          :password       => ENV['SENDGRID_PASSWORD'],
-                                          :domain         => 'heroku.com'
-                                        }
-
-  config.action_mailer.delivery_method = :smtp
-
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
